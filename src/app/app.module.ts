30a165d73d705969b5bbedcf851603964a72730f
Pepijn Schoen <pepijn@eliandor.com>
2017-09-03 15:30:18 +0200

    Include Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
